f244c377d55dd518abf00b0374dd24b47cf72e6d
Mehdi Baneshi <mehdibaneshi1994@gmail.com>
2024-05-31 03:01:46 +0330

    Fix IsTollFreeDate bugs

--- netcore/CongestionTaxCalculator.cs
+++ netcore/CongestionTaxCalculator.cs
@@ -126,16 +126,18 @@ public class CongestionTaxCalculator
         int month = date.Month;
         int day = date.Day;
 
+        if (date.Month == 7) return true;
+
         if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
 
         if (year == 2013)
         {
             if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
+                month == 3 && (day == 28 || day == 29 || day == 31) ||
                 month == 4 && (day == 1 || day == 30) ||
                 month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
+                month == 6 && (day == 5 || day == 6 || day == 20 || day == 21) ||
+                month == 10 && day == 31 ||
                 month == 11 && day == 1 ||
                 month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
             {
