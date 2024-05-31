8a35274ea79eac57e9c76cf6726bda6116e1ef10
Mehdi Baneshi <mehdibaneshi1994@gmail.com>
2024-05-31 04:27:30 +0330

    a minor refactoring

--- netcore/CongestionTaxCalculator.cs
+++ netcore/CongestionTaxCalculator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using CongestionTax.Calculator;
 using CongestionTax.Domain.Contracts.Rules;
 using CongestionTax.Domain.Contracts.Vehicle;
 using CongestionTaxRules;
@@ -20,7 +21,7 @@ public class CongestionTaxCalculator
     {
         if (_vehicle.IsExempt) return 0;
 
-        var distincedLogsByDates = GetDistincedLogsByDates(dates);
+        var distincedLogsByDates = DateHelper.GetDistincedLogsByDates(dates);
         int totalFee = 0;
 
         foreach (var item in distincedLogsByDates)
@@ -40,31 +41,5 @@ public class CongestionTaxCalculator
         int totalTax = _cityCongestionTaxRule.GetTaxApplyingSingleChargeRule(taxes, logs);
 
         return _cityCongestionTaxRule.GetTaxApplyingMaxTaxPerDayRule(totalTax);
-    }
-
-    private Dictionary<DateOnly, List<TimeOnly>> GetDistincedLogsByDates(List<DateTime> logs)
-    {
-        logs=logs.OrderBy(l => l).ToList();
-        var distincedLogsByDates = new Dictionary<DateOnly, List<TimeOnly>>();
-        var oneDaylogs = new List<TimeOnly>();
-        oneDaylogs.Add(TimeOnly.FromDateTime(logs[0]));
-
-        for (int i = 1; i < logs.Count; i++)
-        {
-            if (logs[i].Date == logs[i - 1].Date)
-            {
-                oneDaylogs.Add(TimeOnly.FromDateTime(logs[i]));
-            }
-            else
-            {
-                distincedLogsByDates.Add(DateOnly.FromDateTime(logs[i - 1]), oneDaylogs);
-                oneDaylogs = new List<TimeOnly>();
-                oneDaylogs.Add(TimeOnly.FromDateTime(logs[i]));
-            }
-        }
-
-        distincedLogsByDates.Add(DateOnly.FromDateTime(logs[logs.Count - 1]), oneDaylogs);
-
-        return distincedLogsByDates;
     }
 }
---
+++ netcore/DateHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongestionTax.Calculator
+{
+    public static class DateHelper
+    {
+        public static Dictionary<DateOnly, List<TimeOnly>> GetDistincedLogsByDates(List<DateTime> logs)
+        {
+            logs = logs.OrderBy(l => l).ToList();
+            var distincedLogsByDates = new Dictionary<DateOnly, List<TimeOnly>>();
+            var oneDaylogs = new List<TimeOnly>();
+            oneDaylogs.Add(TimeOnly.FromDateTime(logs[0]));
+
+            for (int i = 1; i < logs.Count; i++)
+            {
+                if (logs[i].Date == logs[i - 1].Date)
+                {
+                    oneDaylogs.Add(TimeOnly.FromDateTime(logs[i]));
+                }
+                else
+                {
+                    distincedLogsByDates.Add(DateOnly.FromDateTime(logs[i - 1]), oneDaylogs);
+                    oneDaylogs = new List<TimeOnly>();
+                    oneDaylogs.Add(TimeOnly.FromDateTime(logs[i]));
+                }
+            }
+
+            distincedLogsByDates.Add(DateOnly.FromDateTime(logs[logs.Count - 1]), oneDaylogs);
+
+            return distincedLogsByDates;
+        }
+    }
+}
